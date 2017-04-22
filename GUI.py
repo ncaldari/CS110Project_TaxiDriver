2a893a74ad01f54ffdd2b22bb54db5b48a5dccda
ncaldari <ncaldari@gmail.com>
2017-04-22 11:57:39 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,9 @@
 import pygame
 import random
-from classes import Taxi, Obstacle
+from classes2 import Taxi, Obstacle
+import pygame.font
+
+pygame.init()
 
 class Taxidriver:
     def __init__(self):
@@ -12,46 +15,51 @@ class Taxidriver:
         blue = (0,0,255)
         green = (0,220,0)
 
+        
         mytaxi = Taxi()
-        myobstacle = Obstacle()
-        myobstacle2 = Obstacle()
+        #myobstacle = Obstacle()
+        #myobstacle2 = Obstacle()
 
 
 
-
-        self.gameView = pygame.display.set_mode((500,500))
-        pygame.display.set_caption('Taxi Driver')
-
-        gameExit = False
-        while not gameExit:
+        self.startView = pygame.display.set_mode((500,500))
+        pygame.display.set_caption('Taxi Driver Start Menu')
+        font = pygame.font.SysFont(None, 50)
+        text_objects = font.render("Taxi Driver", True, green)
+        text_objects2 = font.render("Press Space Bar to Start", True, green)
+        self.startView.blit(text_objects, [150, 100])
+        self.startView.blit(text_objects2, [40, 300])
+        gamestartExit = False
+        while not gamestartExit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    gameExit = True
+                    gamestartExit = True
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        mytaxi.moveLeft()
-                    if event.type == pygame.K_RIGHT:
-                        mytaxi.moveRight()
+                    if event.key == pygame.K_SPACE:
+                        gamestartExit = True
+                        self.gameView = pygame.display.set_mode((500,500))
+                        pygame.display.set_caption('Taxi Driver')
+                        gameExit = False
+                        while not gameExit:
+                            for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                    gameExit = True
+                                if event.type == pygame.KEYDOWN:
+                                    if event.key == pygame.K_LEFT:
+                                        mytaxi.moveLeft()
+                                    if event.type == pygame.K_RIGHT:
+                                        mytaxi.moveRight()
 
-            pygame.draw.rect(self.gameView, blue, mytaxi.shape) #[xcoor,ycoor,width,height]
-        #spawner()
+                            pygame.draw.rect(self.gameView, blue, mytaxi.shape) #[xcoor,ycoor,width,height]
+                            #myobstacle.place
+                            #myobstacle.place
+                            pygame.display.flip()
 
             pygame.display.flip()
 
 
 
-def startMenu(): #this shouldnt be its own function
-    self.startView = pygame.display.set_mode((500,500))
-    gamestartExit = False
-    while not gamestartExit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gamestartExit = True
-        largeText = pygame.font.Font('freesansbold.ttf', 100)
-        TextSurf, TestRect = text_objects("Taxi Driver", largeText)
-        TextRect.center = ((display_width/3), (display_height/3))
-        self.startView.blit(TextSurf,TextRect)
-        pygame.display.update()
+
 
 def main():
     #mytaxi = taxi.Taxi()
