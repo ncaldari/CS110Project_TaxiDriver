66627fb501ad8b35754fcc9d7534421de13af04a
jdong10 <jonathan1025@gmail.com>
2017-04-25 16:52:42 -0400

    Update class2.py (Jon)

--- classes2.py
+++ classes2.py
@@ -1,45 +1,35 @@
 import pygame
 
-#needed to capitalize class names
-class Taxi(pygame.sprite.Sprite):
-	def __init__(self, color='yellow'):
+class GameObj(object):
+	def __init__(self, screen):
+		self.screen = screen
+	
+	def draw(self):
+		pass
+		
+	def update(self):
+		pass
+		
+class Taxi(GameObj):
+	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		#self.xcoor = xcoor
-		#self.ycoor = ycoor
-		#self.start_x = start_x
-		self.color = color
-		self.image = pygame.transform.scale(pygame.image.load('car5.png'), [40,20])
-		#pygame.Rect(xcoor, ycoor, width, height)
+		self.image = pygame.image.load('Taxi_assets/car5.png')
+		self.image = pygame.transform.scale(self.image, (60, 40))
 		self.rect = self.image.get_rect()
-
+		
 		self.rect.x = 250
-		self.rect.y = 460
+		self.rect.y = 0
 
-	def moveLeft(self, left_x = -100):
-		self.left_x = left_x
-		'''
-		if self.start_x != 150:
-			self.start_x -= 100
-			'''
-		print("moving left:", self.rect.x, " ", self.rect.y)
-		if self.rect.x != 150:
-			self.rect = self.rect.move(left_x, 0)
-
-	def moveRight(self, right_x = 100):
-		'''
-		#self.start_x = 250
-		if self.start_x != 350:
-			self.start_x += 100
-			'''
-		print("moving right:", self.shape.x, " ", self.shape.y)
-		if self.rect.x != 350:
-			self.rect = self.rect.move(right_x, 0)
-
-	def __str__(self):
-		mystr = ''
-		mystr += 'Coordinates: ' + str(self.xcoor) + ', ' + str(self.ycoor) + '\n'
-		mystr += 'Color: ' + self.color
-		return mystr
+	def update(self):
+		pass
+	
+	def move(self, l_r):
+		if l_r == 'left'
+			while self.rect.x != 150:
+				self.rect = self.rect.move(150,0)
+		if l_r == 'right'
+			while self.rect.x != 350:
+				self.rect = self.rect.move(350,0)
 
 #we need to integrate the spawning logic into this, might have to change it some
 class Obstacle:
@@ -52,12 +42,3 @@ class Obstacle:
 	def move(self, speed):
 		while self.ycoor >= 0:
 			self.ycoor -= speed
-
-	#def place():
-
-	def __str__(self):
-		mystr = ''
-		mystr += 'Coordinates: ' + str(self.xcoor) + ', ' + str(self.ycoor) + '\n'
-		mystr += 'Color: ' + self.color + '\n'
-		mystr += 'Speed: ' + str(self.speed)
-		return mystr
