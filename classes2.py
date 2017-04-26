28f7f72f79d3bc2491aec7191ca97cb1f500daff
jdong10 <jonathan1025@gmail.com>
2017-04-26 10:23:51 -0400

    Update classes2.py

--- classes2.py
+++ classes2.py
@@ -1,44 +1,44 @@
 import pygame
 
-class GameObj(object):
-	def __init__(self, screen):
-		self.screen = screen
-	
-	def draw(self):
-		pass
-		
-	def update(self):
-		pass
-		
-class Taxi(GameObj):
-	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load('Taxi_assets/car5.png')
-		self.image = pygame.transform.scale(self.image, (60, 40))
-		self.rect = self.image.get_rect()
-		
-		self.rect.x = 250
-		self.rect.y = 0
+class GameObj(pygame.sprite.Sprite):
+    def __init__(self, screen):
+        self.screen = screen
 
-	def update(self):
-		pass
-	
-	def move(self, l_r):
-		if l_r == 'left'
-			while self.rect.x != 150:
-				self.rect = self.rect.move(150,0)
-		if l_r == 'right'
-			while self.rect.x != 350:
-				self.rect = self.rect.move(350,0)
+
+        def update(self):
+            pass
+
+class Taxi(GameObj):
+    def __init__(self):
+        super(GameObj,self).__init__()
+        self.image = pygame.image.load('Taxi_assets/car5.png')
+        self.image = pygame.transform.scale(self.image, (60, 40))
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 250
+        self.rect.y = 0
+        self.pos = 1
+
+    def update(self):
+        pass
+
+# 250 150 350
+    def move(self, l_r):
+        positions = [150,250,350]
+        if l_r == 'left' and self.pos > 0:
+            self.pos -= 1
+        if l_r == 'right' and self.pos < 2:
+            self.pos +=1
+        self.rect.x = positions[self.pos]
+        
 
 #we need to integrate the spawning logic into this, might have to change it some
 class Obstacle:
-	def __init__(self, speed=10, color='gray'):
-		self.color = color
+    def __init__(self, speed=10, color='gray'):
+        self.color = color
+        self.shape = pygame.Rect(xcoor,ycoor, 10, 10)
+        self.speed = speed
 
-		self.shape = pygame.Rect(xcoor,ycoor, 10, 10)
-		self.speed = speed
-
-	def move(self, speed):
-		while self.ycoor >= 0:
-			self.ycoor -= speed
+        def move(self, speed):
+            while self.ycoor >= 0:
+                self.ycoor -= speed
