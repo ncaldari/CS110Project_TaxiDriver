cb4581fa88d609f7145fb3ea5b1b81d3a9b50559
Jonathan D <JonD@Jonathans-MacBook-Pro.local>
2017-04-26 17:02:34 -0400

    taxi render

---
+++ classes.py
@@ -0,0 +1,49 @@
+import pygame
+
+class GameObj(pygame.sprite.Sprite):
+    def __init__(self, screen):
+        self.screen = screen
+
+
+        def update(self):
+            pass
+
+class Taxi(GameObj):
+    def __init__(self):
+        super(GameObj,self).__init__()
+        self.image = pygame.image.load('Taxi_assets/car5.png')
+        self.image = pygame.transform.scale(self.image, (60, 40))
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 250
+        self.rect.y = 460
+        self.pos = 1
+
+    def update(self):
+        pass
+
+# 250 150 350
+    def move(self, l_r):
+        positions = [150,250,350]
+        if l_r == 'left' and self.pos > 0:
+            self.pos -= 1
+        if l_r == 'right' and self.pos < 2:
+            self.pos +=1
+        self.rect.x = positions[self.pos]
+
+
+#we need to integrate the spawning logic into this, might have to change it some
+class Obstacle(GameObj):
+    def __init__(self):
+        super(GameObj, self).__init__()
+        self.image = pygame.image.load('Taxi_assets/car3.png')
+        self.image = pygame.transform.scale(self.image, (60,40))
+        self.rect = self.image.get_rect()
+
+        positions = [150,250,350]
+        self.rect.x = random.choice(positions)
+        self.rect.y = 0
+        self.pos = 1
+
+    def move(self, speed = 5):
+            self.rect = self.rect.move(0, speed)
---
+++ main.py
@@ -0,0 +1,67 @@
+import pygame
+import random
+from classes import Taxi, Obstacle
+import pygame.font
+
+pygame.init()
+
+class Taxidriver:
+    def __init__(self):
+        #pygame.init()
+
+
+        white = (255,255,255)
+        black = (0,0,0)
+        blue = (0,0,255)
+        green = (0,220,0)
+        yellow = (245, 225, 0)
+
+        mytaxi = Taxi()
+        #myobstacle = Obstacle()
+        #myobstacle2 = Obstacle()
+
+        self.startView = pygame.display.set_mode((500,500))
+        pygame.display.set_caption('Taxi Driver Start Menu')
+        font = pygame.font.SysFont(None, 50)
+        text_objects = font.render("Taxi Driver", True, yellow)
+        text_objects2 = font.render("Press Space Bar to Start", True, green)
+        self.startView.blit(text_objects, [150, 100])
+        self.startView.blit(text_objects2, [40, 300])
+        gamestartExit = False
+        while not gamestartExit:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gamestartExit = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        gamestartExit = True
+
+                        #self.gameView = pygame.display.set_mode((500,500))
+                        pygame.display.set_caption('Taxi Driver')
+                        gameExit = False
+                        while not gameExit:
+                            for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                    gameExit = True
+                                if event.type == pygame.KEYDOWN:
+                                    if event.key == pygame.K_LEFT:
+                                        mytaxi.move('left')
+                                    if event.key == pygame.K_RIGHT:
+                                        mytaxi.move('right')
+
+                            self.startView.fill(black)
+                            # draw it write
+                            pygame.draw.rect(self.startView, blue, mytaxi.rect)
+                            #rect.draw(startView)
+                            pygame.display.flip()
+
+            pygame.display.flip()
+
+
+
+
+
+def main():
+    #mytaxi = taxi.Taxi()
+    Taxidriver()
+main()
